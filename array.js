18c2bf61f90ca45febdaf6d6338a822092d0595e
programmershakib <programmershakibinfo@gmail.com>
2024-09-19 15:50:26 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+// const product = [
+//     {name: 'phone', price: 12000},
+//     {name: 'laptop', price: 32000},
+// ];
+
+// product[0];
+// product[1];
+
+// const product = [15, 56, 87];
+const product = {
+    '0': 15,
+    '1': 56,
+    '2': 87,
+}
